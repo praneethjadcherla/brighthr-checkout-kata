5f13ad626c40deabd49bb68d9387a0b752dba016
Praneeth <praneethjadcherla96@gmail.com>
2024-03-20 01:30:45 +0000

    initial commit basic test case passed

---
+++ ClassLibrary1/ClassLibrary1/main/Checkout.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.main
+{
+    public class Checkout : ICheckout
+    {
+        public int GetTotalPrice()
+        {
+            int totalprice = 0;
+
+            return totalprice;
+        }
+
+        public void Scan(string item)
+        {
+            
+        }
+    }
+}
---
+++ ClassLibrary1/ClassLibrary1/test/CheckoutTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary.main;
+using NUnit.Framework;
+
+namespace ClassLibrary.test
+{
+    [TestFixture]
+    public class CheckoutTests
+    {
+        [Test]
+        public void GetTotalPrice_ReturnsZero_WhenNoItemsScanned()
+        {
+            // Arrange
+            var checkout = new Checkout();
+            
+
+            // Act
+            var totalPrice = checkout.GetTotalPrice();
+
+            // Assert
+            Assert.That(totalPrice, Is.EqualTo(0));
+        }
+    }
+}
